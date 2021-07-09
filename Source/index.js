cd741d747670e84ea53b82b7d1c13378651753e0
Sarvjeet Rajvansh <rajvanshsarvjeet@gmail.com>
2021-07-09 18:55:10 +0530

    Refactor code and css update for mobil dev

--- Source/index.js
+++ Source/index.js
@@ -2,25 +2,31 @@ var btnConvert=document.querySelector("#btn-convert");
 var txtInput=document.querySelector("#txt-input");
    
 function clickHandler(){
-    document.getElementById("txt-output").innerHTML="Processing.....";
+
+    setMessage("txt-output","Processing.....");
    
     if(txtInput.value=="" || txtInput.value==null || txtInput.value==undefined ){
 
-        document.getElementById("errorLabel").innerHTML="*please enter your thoughts first";
-        document.getElementById("txt-output").innerHTML="Meh!! No thoughts to translate...";
+        setMessage("errorLabel","*please enter your thoughts first");
+        setMessage("txt-output","Meh!! No thoughts to translate...");
         return;
     }
-         // txtOutput=txtInput.value;
-        // document.getElementById("txt-output").innerHTML=txtOutput;
-         fetch(`https://api.funtranslations.com/translate/minion.json?text=${txtInput.value}`)
+       
+         fetch(urlConstructor(txtInput.value))
         .then(response => response.json())
-        .then(data => {document.getElementById("txt-output").innerHTML=data.contents.translated.toString()});
+        .then(data => {setMessage("txt-output",data.contents.translated.toString())})
+}
+
+function urlConstructor(input){
+    return "https://api.funtranslations.com/translate/minion.json?text="+input;
+}
+
+function setMessage(id,Message){
+    document.getElementById(id).innerHTML=Message;
 }
 function cleanError(){
     document.getElementById("errorLabel").innerHTML="";
-    document.getElementById("txt-output").innerHTML="";
-
+   document.getElementById("txt-output").innerHTML="";
 }
-
 txtInput.addEventListener("input",cleanError);
 btnConvert.addEventListener("click",clickHandler);
